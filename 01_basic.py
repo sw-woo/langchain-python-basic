6e0e9e766fb45001e0127460c2ec2c31911a21cc
sungwoo <swpheus1@gmail.com>
2024-12-31 13:25:35 +0900

    fix:내용 합치는 부분 수정 01_basic.py

--- 01_basic.py
+++ 01_basic.py
@@ -13,7 +13,8 @@ if OPENAI_API_KEY is None:
 chat = ChatOpenAI(
     # or gpt-4 oepn-ai 모델 선택부분은 공식 홈페이지에서 확인 가능합니다. 현재는 gpt-3.5-turbo 모델을 사용하겠습니다.
     model="gpt-3.5-turbo",
-    temperature=0, openai_api_key=OPENAI_API_KEY
+    temperature=0, 
+    openai_api_key=OPENAI_API_KEY
 )
 
 # 질문 정의
